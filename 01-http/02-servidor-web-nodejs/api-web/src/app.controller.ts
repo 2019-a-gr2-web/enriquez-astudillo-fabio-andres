bd3fbe1d004db070257dcbd15dae475c146d3436
Fabio <fabio.and1514@gmail.com>
2019-04-17 15:57:49 -0500

    request

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,47 +1,140 @@
-import {Controller, Delete, Get, HttpCode, Post, Put, Headers} from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get, HttpCode, Post, Put, Delete, Headers, Param, Request, Response} from '@nestjs/common';
+import {AppService} from './app.service';
+import {Body} from "@nestjs/common/decorators/http/route-params.decorator";
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+import cookieParser = require('cookie-parser');
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  await app.listen(3000);
+}
+
+bootstrap();
+
+// http://192.168.1.10:3000/segmentoInicial/segmentoAccion
+// http://192.168.1.10:3000/mascotas/crear
+// http://192.168.1.10:3000/mascotas/borrar
+// @Controller(segmentoInicial)
 @Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
+  // @Controller(segmentoAccion)
   @Get('/hello-world')  // METODO HTTP
-  getHello(): string {
-     return 'hola mundo'
+  helloWorld(): string {
+    return 'Hello world';
   }
 
-  @Get('/adivina')
-  adivina(@Headers() headers): string {
-      console.log('Headers: ',headers);
-      const numeroRandomico = Math.round(Math.random()*10);
-      return 'Ok';
-  }
-
+  // POST http://localhost:3000/api
   @Post('/hola-mundo')  // METODO HTTP
-  postHello(): string{
-    return 'hola mundo';
-  }
-  @Put('/helo')
-  putHello(): string{
-    return 'helo'
-  }
-  @Delete('/ola')
-  deleteHello(): string{
-    return 'ola'
+  holaMundo() {
+    return 'Hola mundo';
   }
 
+  @Get('/ciudad/:idCiudad')
+  ciudad(@Param() parametrosRuta){
+    switch(parametrosRuta.idCiudad.toLowerCase()) {
+      case 'quito':
+        return 'que fuef';
+      case 'guayaquil':
+        return 'Que ma ñañosh';
+      default:
+        return 'Buenas Tardes';
+    }
+  }
+
+  @Post('/registroComida')
+  registroComida(
+      @Body() parametrosCuerpo,
+      @Response() response
+
+  ){
+    if(parametrosCuerpo.nombre && parametrosCuerpo.cantidad){
+      const cantidad = Number(parametrosCuerpo.cantidad);
+      if(cantidad > 1){
+        response.set('Premio','Fanesca');
+      }
+      return response.send({mensaje:'Registro Creado'})
+    }else{
+      return response.status(400)
+          .send({
+            mensaje:'Error: no envía nombre o cantidad',
+            error:400
+          });
+    }
+  }
+
+  @Get('/semilla')
+  semilla(@Request() request) {
+    console.log(request.cookies);
+    const cookies = request.cookies;
+    if (cookies.micookie) {
+      return 'ok'
+    } else {
+      return ':('
+    }
+  }
+
+  @Put('/salut-monde')  // METODO HTTP
+  salutMonde() {
+    return 'Salut monde';
+  }
+
+  @Delete('/ola-mundo')  // METODO HTTP
+  olaMundo() {
+    return 'Olá mundo';
+  }
+
+
+
+  @Get('/adivina')  // METODO HTTP
+  adivina(@Headers() headers): string {
+    console.log('Headers: ', headers);
+    const numeroRandomico =  Math.round(Math.random()*10);
+    const numeroDeCabecera = Number(headers.numero);
+
+    if( numeroDeCabecera == numeroRandomico){
+      return 'Ok';
+    }else{
+      return ':(';
+    }
+
+
+  }
+
+
+  // js -> ts
+
+
+
+
+
+
+  /*
+  const nombre: string = 'Adrian'; // string
+  const edad = 29;  // number
+  const sueldo = 1.20;  // number
+  const casado = false;  // boolean
+  const hijos = null;  // null
+  const alas = undefined;  // undefined
+  */
+
+
+  /*
+  * Segmento inicial: /api
+  * 1) Segmento Accion: GET 'hello-world' -> 'Hello world'
+  * 2) Segmento Accion: POST 'hola-mundo' -> 'Hola mundo'
+  * 3) Segmento Accion: PUT '...' -> '....'
+  * 4) Segmento Accion: DELETE '..' -> '....'
+  * */
 
 
 }
-const json = [
-  {llave:"valor"}
-];
-let obketo = {
-  propiedad: 'valor',
-  propiedad2: 'valor2'
-};
-objeto.propiedad
-objeto.propiedad2
+
+
 /*
 @NombreDecoradorClase() // Decorador -> FUNCION
 class usuario{
@@ -63,3 +156,47 @@ class usuario{
   protected metodoProtegido(){}
 }
 */
+
+const json = [
+  {
+    llave: 'valor',
+    "key": "value",
+    'nombre': "Adrian\"\"",
+    edad: 29,
+    sueldo: 10.21,
+    casado: false,
+    hijos: null,
+    mascotas: [
+      "cachetes",
+      1,
+      1.01,
+      false,
+      null,
+      {
+        "nombre":"cachetes"
+      },
+    ],
+  },
+];
+
+// JS -> JSON
+
+let adrian = 'Adrian';
+
+// TS
+
+let vicente:any = 'Vicente';
+vicente = 1;
+
+let objeto:any = {
+  propiedad:'valor',
+  propiedadDos:'valor2'
+};
+objeto.propiedad  // valor
+objeto.propiedadDos  // valor2
+
+// Agregar propiedades a un objeto
+objeto.propiedadTres = 'valor3';
+objeto['propiedadTres'] = 'valor 3';
+delete objeto.propiedadTres; // -> destruir
+objeto.propiedadTres = undefined; // -> destruir
