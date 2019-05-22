eca0b97d2ef3e87603c1871426f46ff9ba4acecb
Fabio <fabio.and1514@gmail.com>
2019-05-22 16:02:23 -0500

    app web

--- 01-http/02-servidor-web-nodejs/api-web/src/tragos/tragos.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/tragos/tragos.controller.ts
@@ -1,5 +1,6 @@
-import {Controller, Get, Response} from "@nestjs/common";
+import {Controller, Get, Response, Post, Body} from "@nestjs/common";
 import {TragosService} from "./tragos.service";
+import { Trago } from "./interfaces/Trago";
 
 @Controller('/api/traguito')
 export class TragosController {
@@ -15,4 +16,42 @@ export class TragosController {
             arregloTragos:arregloTragos
         })
     }
+
+    @Get('crear')
+    crearTragos(@Response() res){
+        res.render('tragos/crear-editar')
+    }
+
+    @Post('crear')
+    crearTragoPost(
+        @Body() trago:Trago,
+        @Body('nombre') nombre:string,
+        @Body('tipo') tipo:string,
+        @Body('gradosAlcohol') gradosAlcohol:number,
+        @Body('fechaCaducidad') fechaCaducidad:Date,
+        @Body('precio') precio:number,
+        @Response() res
+    ){
+        trago.gradosAlcohol = Number(trago.gradosAlcohol);
+        trago.precio = Number(trago.precio);
+        trago.fechaCaducidad = new Date(trago.fechaCaducidad);
+        //console.log(trago);
+
+        this._tragosService.crear(trago);
+        res.redirect('/api/traguito/lista')
+
+        // console.log('Trago: ', trago, typeof trago);
+        // console.log('Nombre: ', nombre, typeof nombre);
+        // console.log('Tipo: ', tipo, typeof tipo);
+        // console.log('GradosAlcohol: ', gradosAlcohol, typeof gradosAlcohol);
+        // console.log('FechaCaducidad: ', fechaCaducidad, typeof fechaCaducidad);
+        // console.log('Precio: ', precio, typeof precio);
+
+    }
+
+    @Post('eliminar')
+    eliminarTragoPost(@Body() id:number){
+        this._tragosService.eliminar(id);
+    }
+
 }
